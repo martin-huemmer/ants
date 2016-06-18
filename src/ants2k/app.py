c13461c093e60c4de343820b6388dea4182a6ebc
mhuemmer <mhuemmer@feinarbyte.de>
2016-06-18 23:20:41 +0200

    divide kivy app in two timers

--- src/ants2k/app.py
+++ src/ants2k/app.py
@@ -1,40 +1,52 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
-from ant import Ant
+from kivy.properties import ObjectProperty, NumericProperty
+from kivy.core.window import Window
+from kivy.graphics import Color, Rectangle
 from kivy.clock import Clock
 from position import Position
+from hive import Hive
 
+class AntHive(Widget):
 
-class AntPixel(Widget):
+    def createHive(self, size):
+        self.hive = Hive(Position(Window.size[0]/2,Window.size[1]/2), size)
 
-    antObject = ObjectProperty(None)
-    antObject = Ant(Position(400,400))
-
-    def move(self):
-        self.antObject.move()
-
-    def draw(self):
-        self.pos = self.antObject.pos.returnTuple()
 
 class HiveCanvas(Widget):
 
-    ant = ObjectProperty(None)
+    hiveObject = ObjectProperty(None)
+
+    def createHiveInstance(self, size):
+       self.hiveObject.createHive(size)
 
     # update canvas
     def draw(self, dt):
-        self.ant.draw()
+        self.hiveObject.canvas.clear()
+
+        for ant in self.hiveObject.hive.ants:
+
+            with self.hiveObject.canvas:
+                # Add a white color
+                Color(1, 1, 1)
+                # Add a rectangle
+                Rectangle(pos=ant.pos.returnTuple(), size=(1, 1))
 
     # update position
     def tick(self, dt):
-        self.ant.move()
+        for ant in self.hiveObject.hive.ants:
+            foo = ant
+            pos = foo.pos
+            ant.move()
+            test = ant.pos
 
 
 class SimApp(App):
     def build(self):
         canvas = HiveCanvas()
         # call update position calculation
-        Clock.schedule_interval(canvas.tick, .1)
+        canvas.createHiveInstance(5)
+        Clock.schedule_interval(canvas.tick, 1)
         # call redrawing of canvas
         Clock.schedule_interval(canvas.draw, 1)
         return canvas

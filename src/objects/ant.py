8138beb96efd3046294f094010c29bc8107d1e8e
mhuemmer <mhuemmer@feinarbyte.de>
2016-06-18 17:15:20 +0200

    removed homing for ant

--- src/objects/ant.py
+++ src/objects/ant.py
@@ -11,15 +11,9 @@ from src.utils import directions
 
 class Ant:
 
-    def __init__(self, name, home, pheromoneMap):
-        self.name = name
-        self.home = home
-        self.homing = False
-        # only pass value
-        xHome = home[0]
-        yHome = home[1]
-
-        self.pos = [xHome, yHome]
+    def __init__(self, id, pos, pheromoneMap):
+        self.id = id
+        self.pos = pos
         self.pheromoneMap = pheromoneMap
         self.maxX, self.maxY = self.pheromoneMap.size
         self.minX, self.minY = [0,0]
@@ -36,45 +30,23 @@ class Ant:
                 self.heading = self.heading - 45
 
     def roam(self):
-        if self.pos[0] > 500:
-            self.homing = True
-        if self.homing:
-            self.comeHome()
-        else:
-            self.setDirection()
-            direction = self.heading % 360
-            # walk one step in direction
-            self.pos = directions[direction](self.pos)
-            # turn arround when reaching the border
-            if self.pos[0] >= self.maxX:
-                self.pos[0] = self.maxX - 1
-            if self.pos[0] <= self.minX:
-                self.pos[0] = 1
-            if self.pos[1] >= self.maxY:
-                self.pos[1] = self.maxY - 1
-            if self.pos[1] <= self.minY:
-                self.pos[1] = 1
-            self.excretePheromones()
 
-        return self.pos
-
-    def comeHome(self):
-        # get home
-        if self.pos[0] > self.home[0]:
-            self.pos[0] = self.pos[0] - 1
-        elif self.pos[0] < self.home[0]:
-            self.pos[0] = self.pos[0] + 1
-        else:
-            self.pos[0] = self.pos[0]
-
-        if self.pos[1] > self.home[1]:
-            self.pos[1] = self.pos[1] - 1
-        elif self.pos[1] < self.home[1]:
-            self.pos[1] = self.pos[1] + 1
-        else:
-            self.pos[1] = self.pos[1]
+        self.setDirection()
+        direction = self.heading % 360
+        # walk one step in direction
+        self.pos = directions[direction](self.pos)
+        # turn arround when reaching the border
+        if self.pos[0] >= self.maxX:
+            self.pos[0] = self.maxX - 1
+        if self.pos[0] <= self.minX:
+            self.pos[0] = 1
+        if self.pos[1] >= self.maxY:
+            self.pos[1] = self.maxY - 1
+        if self.pos[1] <= self.minY:
+            self.pos[1] = 1
         self.excretePheromones()
 
+        return self.pos
 
     def excretePheromones(self):
         self.pheromoneMap.map[self.pos[0]][self.pos[1]] = self.pheromoneMap.map[self.pos[0]][self.pos[1]] + 1
